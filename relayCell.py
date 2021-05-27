c5af7b1efd8859746080ba73a4b59d0bfb0f7a49
craig <kelley92@bu.edu>
2021-05-27 10:37:00 -0400

    no more template

--- relayCell.py
+++ relayCell.py
@@ -1,7 +1,6 @@
 from neuron import h
 h.load_file('geom_pyr.hoc')
 
-class relayCell():
+class relay():
     def __init__(self):
-        self.cell = h.relay()
-        
+        self.cell = h.init()
